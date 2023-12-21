e38b8f2bab78ae184f7c947f1824999bacc26679
IGNRV <lgi.rodriguezvillarroel@gmail.com>
2023-12-21 21:53:42 +0000

    Avance historial_cambios pago anual cheques

--- procesar_pago_cheques.php
+++ procesar_pago_cheques.php
@@ -2,6 +2,8 @@
 require_once 'db.php'; 
 /* ini_set('display_errors', 1); */
 
+session_start();
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $json = file_get_contents('php://input');
     $data = json_decode($json);
@@ -13,8 +15,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $stmtFolio->execute();
         $resultFolio = $stmtFolio->get_result();
         $ultimoFolio = $resultFolio->fetch_assoc();
-        $folioActual = isset($ultimoFolio['FOLIO_PAGO']) ? $ultimoFolio['FOLIO_PAGO'] : 0; // Si no hay filas, empieza desde 0
+        $folioActual = isset($ultimoFolio['FOLIO_PAGO']) ? $ultimoFolio['FOLIO_PAGO'] : 0;
         $stmtFolio->close();
+
         foreach ($data->pagos as $pago) {
             $folioActual++;
             $fechaEmision = new DateTime($pago->fechaEmision);
@@ -22,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $stmt = $conn->prepare("INSERT INTO DETALLES_TRANSACCION 
             (FOLIO_PAGO, ANO, VALOR, FECHA_PAGO, MEDIO_DE_PAGO, ESTADO, TIPO_DOCUMENTO, NUMERO_DOCUMENTO, FECHA_EMISION, FECHA_COBRO, BANCO, N_CUOTAS, ID_PAGO, CTA_CORRIENTE) 
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("iidssisssssisi", 
+            $stmt->bind_param("iidssisssssisi", 
                 $folioActual,
                 $pago->ano,
                 $pago->monto,
@@ -32,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $pago->tipoDocumento,
                 $pago->nDocumento,
                 $pago->fechaEmision,
-                $fechaCobro,  // Usar la fecha calculada
+                $fechaCobro,
                 $pago->banco,
                 $pago->nCuotas,
                 $pago->idPago,
@@ -40,45 +43,63 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             );
             $stmt->execute();
 
-            // Consulta para obtener el valor actual pagado
             $query = $conn->prepare("SELECT VALOR_PAGADO FROM HISTORIAL_PAGOS WHERE ID_PAGO = ?");
             $query->bind_param("i", $pago->idPago);
             $query->execute();
             $resultado = $query->get_result();
-            if ($fila = $resultado->fetch_assoc()) {
-                $valorActual = $fila['VALOR_PAGADO'];
-            } else {
-                $valorActual = 0; // O manejar el caso de no encontrar el pago
-            }
+            $valorActual = $resultado->fetch_assoc()['VALOR_PAGADO'] ?? 0;
             $query->close();
 
-            // Calcula el nuevo valor
             $nuevoValor = $valorActual + $pago->monto;
 
-            // Actualiza VALOR_PAGADO sumando el nuevo valor
             $updateStmt = $conn->prepare("UPDATE HISTORIAL_PAGOS SET VALOR_PAGADO = ? WHERE ID_PAGO = ?");
             $updateStmt->bind_param("di", $nuevoValor, $pago->idPago);
             $updateStmt->execute();
             $updateStmt->close();
 
-            // Consulta para obtener el VALOR_A_PAGAR
             $queryValorAPagar = $conn->prepare("SELECT VALOR_A_PAGAR FROM HISTORIAL_PAGOS WHERE ID_PAGO = ?");
             $queryValorAPagar->bind_param("i", $pago->idPago);
             $queryValorAPagar->execute();
             $resultadoValorAPagar = $queryValorAPagar->get_result();
-            if ($filaValorAPagar = $resultadoValorAPagar->fetch_assoc()) {
-                $valorAPagar = $filaValorAPagar['VALOR_A_PAGAR'];
-            } else {
-                $valorAPagar = 0; // O manejar el caso de no encontrar el pago
-            }
+            $valorAPagar = $resultadoValorAPagar->fetch_assoc()['VALOR_A_PAGAR'] ?? 0;
             $queryValorAPagar->close();
 
-            // Compara VALOR_PAGADO con VALOR_A_PAGAR y actualiza ESTADO_PAGO
-            $estadoPago = ($nuevoValor == $valorAPagar) ? 2 : 3;
+            $estadoPago = ($nuevoValor >= $valorAPagar) ? 2 : 3;
+
             $updateEstadoPagoStmt = $conn->prepare("UPDATE HISTORIAL_PAGOS SET ESTADO_PAGO = ? WHERE ID_PAGO = ?");
             $updateEstadoPagoStmt->bind_param("ii", $estadoPago, $pago->idPago);
             $updateEstadoPagoStmt->execute();
             $updateEstadoPagoStmt->close();
+        }
+
+        // Obtener el ID del usuario actual
+        $EMAIL = $_SESSION['EMAIL'];
+        $queryUsuario = "SELECT ID FROM USERS WHERE EMAIL = ?";
+        $stmtUsuario = $conn->prepare($queryUsuario);
+        $stmtUsuario->bind_param("s", $EMAIL);
+        $stmtUsuario->execute();
+        $resultadoUsuario = $stmtUsuario->get_result();
+        $idUsuario = null;
+        if ($filaUsuario = $resultadoUsuario->fetch_assoc()) {
+            $idUsuario = $filaUsuario['ID'];
+        }
+        $stmtUsuario->close();
+
+        if (!$idUsuario) {
+            throw new Exception("No se pudo obtener el ID del usuario.");
+        }
+
+        // Insertar en la tabla HISTORIAL_CAMBIOS
+        foreach ($data->pagos as $pago) {
+            $tipoCambio = "REGISTRO DE PAGO CON CHEQUE ANUAL";
+            $stmtHistorial = $conn->prepare("INSERT INTO HISTORIAL_CAMBIOS (ID_USUARIO, TIPO_CAMBIO, ID_ALUMNO) VALUES (?, ?, ?)");
+            $stmtHistorial->bind_param("isi", $idUsuario, $tipoCambio, $pago->idAlumno);
+            $stmtHistorial->execute();
+
+            if ($stmtHistorial->affected_rows == 0) {
+                throw new Exception("Error al insertar en HISTORIAL_CAMBIOS.");
+            }
+            $stmtHistorial->close();
         }
 
         $conn->commit(); // Si todo salió bien, confirma los cambios
